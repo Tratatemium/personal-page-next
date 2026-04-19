165e0b080eda08a5aa6b6d270713b72c815fe488
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-04-19 18:10:36 +0200

    feat: enhance About section with new content and improve styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,1 +1,35 @@
 import styles from "./page.module.css";
+
+import { MyPhoto } from "@/components/MyPhoto";
+
+function About({ PhotoComponent = MyPhoto }) {
+  return (
+    <>
+      <h1 className={styles.title}>&lt;About me&gt;</h1>
+
+      <PhotoComponent className={styles.myPhoto} />
+
+      <section className={styles.text}>
+        <p>
+          Hi, I&apos;m Vladislav Zhuravskii, a front-end development student at
+          Hyper Island, Stockholm Sweden. Originally from Moscow, I&apos;ve
+          lived in Sweden since 2022 and I&apos;m building a new career in tech
+          after working in logistics and warehouse management.
+        </p>
+        <p>
+          I enjoy creating clean, user-friendly web interfaces and continue to
+          expand my skills in programming and problem-solving. With previous
+          experience in Python, Lua, and web technologies, I&apos;m passionate
+          about turning ideas into interactive projects and growing as a
+          developer.
+        </p>
+        <p>
+          Outside of coding, I enjoy hiking, cooking, and exploring creative
+          hobbies.
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default About;
