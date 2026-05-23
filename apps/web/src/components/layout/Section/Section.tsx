b1431f9a50a310aca28cdafce5c8f05807d76c9f
Vladislav Zhuravskii <91178491+Tratatemium@users.noreply.github.com>
2026-05-23 14:46:02 +0200

    Refacor/studio (#4)
    * Implement code changes to enhance functionality and improve performance
    
    * Add Sanity schema and TypeScript types for gallery and project documents
    
    * Add Sanity client, queries, and types for improved data handling
    
    * Fix import path for Sanity client in Projects component
    
    * Add revalidation API route and environment variable utility function
    
    * Refactor Projects component to use getProjects fetcher and simplify Section component props
    
    * Refactor Navbar component to use a links array for cleaner code and improved maintainability
    
    * Remove duplicate dev:studio script from package.json

--- apps/web/src/components/layout/Section/Section.tsx
+++ apps/web/src/components/layout/Section/Section.tsx
@@ -1,42 +1,85 @@
 "use client";
 
+import type { PortableTextComponents } from "@portabletext/react";
+import { PortableText } from "@portabletext/react";
+import { getImageDimensions } from "@sanity/asset-utils";
+import { Image } from "next-sanity/image";
+
 import styles from "./Section.module.css";
-import type { ImageType } from "../../feature/Gallery/Gallery";
 import { ArrowLink } from "../../ui/ArrowLink/ArrowLink";
-import { Gallery } from "../../feature/Gallery/Gallery";
 
 import { useInView } from "@/hooks/useInView";
 
+import type { PROJECTS_QUERY_RESULT } from "@/lib/sanity/sanity.types";
+import { urlFor } from "@/lib/sanity/client";
+
+type Project = PROJECTS_QUERY_RESULT[number];
 interface SectionProps {
-  title: string;
-  body: string[];
-  links?: {
-    href: string;
-    text: string;
-    svgType: "GitHub" | "Globe";
-  }[];
-  images?: ImageType[];
+  project: Project;
 }
 
-function Section({ title, body, links, images }: SectionProps) {
+// --- Portable Text components ---
+
+const components: PortableTextComponents = {
+  block: {
+    h3: ({ children }) => <h3>{children}</h3>,
+    h4: ({ children }) => <h2>{children}</h2>,
+    normal: ({ children }) => <p>{children}</p>,
+    blockquote: ({ children }) => <blockquote>{children}</blockquote>,
+  },
+  types: {
+    image: ({ value }) => {
+      const { width, height } = getImageDimensions(value);
+      return (
+        <div className={styles.image}>
+          <Image
+            src={urlFor(value).url()}
+            alt={value.alt || ""}
+            width={width}
+            height={height}
+          />
+        </div>
+      );
+    },
+    // gallery: ({ value }: { value: Gallery }) => (
+    //   <BlogGallery rawImages={value.images} />
+    // ),
+  },
+  marks: {
+    link: ({ children, value }) => (
+      <a
+        className={styles.link}
+        href={value.href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {children}
+      </a>
+    ),
+  },
+};
+
+function Section({ project }: SectionProps) {
   const { ref, isInView, isScrollingUp } = useInView();
   return (
     <section
       ref={ref}
       className={`${styles.section} ${isInView ? styles.show : isScrollingUp ? styles.hideUp : styles.hideDown}`}
     >
-      <h2 className={styles.title}>{title}</h2>
-      {body.map((block, i) => (
-        <p key={i}>{block}</p>
-      ))}
-      {links && (
+      <h2 className={styles.title}>{project.title}</h2>
+
+      {project.body && (
+        <PortableText value={project.body} components={components} />
+      )}
+
+      {project.links && (
         <div className={styles.links}>
-          {links.map((link) => (
-            <ArrowLink key={link.href} {...link} />
+          {project.links.map((link) => (
+            <ArrowLink key={link._key} link={link} />
           ))}
         </div>
       )}
-      {images && <Gallery images={images}></Gallery>}
+      {/* {images && <Gallery images={images}></Gallery>} */}
     </section>
   );
 }
