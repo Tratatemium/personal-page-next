6d4c9c1d611da71bd477d0cea7d11c74d41e1a68
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-05-23 12:27:50 +0200

    Add revalidation API route and environment variable utility function

--- apps/web/src/lib/sanity/client.ts
+++ apps/web/src/lib/sanity/client.ts
@@ -3,8 +3,8 @@ import { createImageUrlBuilder } from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url";
 
 const client = createClient({
-  projectId: "vsd49p5h",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: "2024-01-01",
   useCdn: false,
 });
