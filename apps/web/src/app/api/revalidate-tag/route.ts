8e1d9a61a06ad20eca247e3ce5ed271b6022b232
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-05-24 11:20:58 +0200

    Refactor webhook handler for improved readability and logging

--- apps/web/src/app/api/revalidate-tag/route.ts
+++ apps/web/src/app/api/revalidate-tag/route.ts
@@ -6,38 +6,39 @@ import { handleRevalidation } from "@/lib/sanity/revalidate";
 type WebhookPayload = { _type: string };
 
 export async function POST(req: NextRequest) {
-    try {
-        if (!process.env.SANITY_REVALIDATE_SECRET) {
-            return new Response(
-                "Missing environment variable SANITY_REVALIDATE_SECRET",
-                { status: 500 },
-            );
-        }
-
-        const { isValidSignature, body } = await parseBody<WebhookPayload>(
-            req,
-            process.env.SANITY_REVALIDATE_SECRET,
-            true,
-        );
-
-        if (!isValidSignature) {
-            return new Response(
-                JSON.stringify({ message: "Invalid signature" }),
-                { status: 401 },
-            );
-        }
-
-        if (!body?._type) {
-            return new Response(
-                JSON.stringify({ message: "Bad Request", body }),
-                { status: 400 },
-            );
-        }
-
-        handleRevalidation(body._type);
-        return NextResponse.json({ body });
-    } catch (err: unknown) {
-        const message = err instanceof Error ? err.message : "Unknown error";
-        return new Response(message, { status: 500 });
+  try {
+    if (!process.env.SANITY_REVALIDATE_SECRET) {
+      return new Response(
+        "Missing environment variable SANITY_REVALIDATE_SECRET",
+        { status: 500 },
+      );
     }
+
+    const { isValidSignature, body } = await parseBody<WebhookPayload>(
+      req,
+      process.env.SANITY_REVALIDATE_SECRET,
+      true,
+    );
+
+    console.log(process.env.SANITY_REVALIDATE_SECRET);
+    console.log(req);
+
+    if (!isValidSignature) {
+      return new Response(JSON.stringify({ message: "Invalid signature" }), {
+        status: 401,
+      });
+    }
+
+    if (!body?._type) {
+      return new Response(JSON.stringify({ message: "Bad Request", body }), {
+        status: 400,
+      });
+    }
+
+    handleRevalidation(body._type);
+    return NextResponse.json({ body });
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : "Unknown error";
+    return new Response(message, { status: 500 });
+  }
 }
